7103fb9d7487d4d7a65c84dc45d1a186c74e80b1
Cristina Garcia <garmercy.mar@gmail.com>
2024-01-14 13:46:48 +1100

    adding links to portfolio and img edited

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,7 +3,7 @@ import photo2 from '../assets/img/5656.jpg';
 import photo3 from '../assets/img/gameGenius.jpg';
 import photo4 from '../assets/img/jate.jpg';
 import photo5 from '../assets/img/password.jpg';
-import photo6 from '../assets/img/baby.jpg';
+import photo6 from '../assets/img/baby2.jpg';
 
 export default function Portfolio() {
     return (
@@ -11,29 +11,18 @@ export default function Portfolio() {
     <div>
       <h1 id='h1Portfolio' class="text-center display-4">Portfolio</h1>
       <div class="row">
-        <div class="col-lg-4 col-md-6 mb-3">
-          <div id='containerCardPortfolio' class="card h-100">
-          <img src={photo1} alt="logo" />
-            <div id='containerTextCard' class="card-body">
-              <a href="">
-                <h2 id='titleCard' class="card-title">Work Day Scheduler</h2>
-                <p id='textCard' class="card-text">I created a calendar application by modifying starter code.</p>
-                <p id='textCard'>The goals of this project are:</p>
-                <p id='textCard'>Allows an user to save events for each hour of a typical working day (9am–5pm).</p>
-                <p id='textCard'>Feature dynamically updated HTML and CSS powered by jQuery and run in the browser.</p>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6 mb-4">
+      <div class="col-lg-4 col-md-6 mb-4">
           <div id='containerCardPortfolio' class="card h-100">
           <img src={photo2} alt="logo" />
             <div id='containerTextCard' class="card-body">
-              <a href="">
                 <h2 id='titleCard' class="card-title">Weather Dashboard</h2>
-                <p id='textCard' class="card-text">The main goal of this project it's build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
-The weather dashboard will contain all the cities weather information and will be used APIs in this project to get the weather information.</p>
-              </a>
+                <p id='textCard' class="card-text">The main goal of this project it's build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.</p>
+                <div>
+                  <a id='linkPortfolio' href="https://garmercy.github.io/Weather-Dashboard/">Deployed application</a>
+                </div>
+                <div>
+                  <a id='linkPortfolio'  href="https://github.com/garmercy/Weather-Dashboard?tab=readme-ov-file">GitHub repository</a>
+                </div>
             </div>
           </div>
         </div>
@@ -41,10 +30,45 @@ The weather dashboard will contain all the cities weather information and will b
           <div id='containerCardPortfolio' class="card h-100">
           <img src={photo3} alt="logo" />
             <div id='containerTextCard' class="card-body">
-              <a href="">
                 <h2 id='titleCard' class="card-title">GameGenius Project1</h2>
                 <p id='textCard' class="card-text">GameGenius is a user-friendly platform that provides tailored game recommendations to players. Relish in personalized suggestions, an extensive game library. Unearth exceptional games effortlessly with GameGenius.</p>
-              </a>
+                <div>
+                  <a id='linkPortfolio' href="https://muhammadkhabbaz.github.io/GameGenius/index.html">Deployed application</a>
+                </div>
+                <div>
+                  <a id='linkPortfolio' href="https://github.com/garmercy/GameGenius?tab=readme-ov-file">GitHub repository</a>
+                </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div id='containerCardPortfolio' class="card h-100">
+          <img src={photo6} alt="logo" />
+            <div id='containerTextCard' class="card-body">
+                <h2 id='titleCard' class="card-title">Project 1</h2>
+                <p id='textCard' class="card-text">Babysitters Club is a community-driven platform designed to facilitate a trusted and secure environment for parents to exchange babysitting services.</p>
+                <div>
+                  <a id='linkPortfolio' href="https://babysitters-app-7fe0f50cf2a3.herokuapp.com/">Deployed application</a>
+                </div>
+                <div>
+                  <a id='linkPortfolio' href="https://github.com/honguyen00/babysitters">GitHub repository</a>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div id='containerCardPortfolio' class="card h-100">
+          <img src={photo1} alt="logo" />
+            <div id='containerTextCard' class="card-body">
+                <h2 id='titleCard' class="card-title">Work Day Scheduler</h2>
+                <p id='textCard' class="card-text">description</p>
+                <div>
+                  <a id='linkPortfolio' href="https://garmercy.github.io/Work-Day-Scheduler/">Deployed application</a>
+                </div>
+                <div>
+                  <a id='linkPortfolio' href="https://github.com/garmercy/Work-Day-Scheduler?tab=readme-ov-file">GitHub repository</a>
+                </div>
             </div>
           </div>
         </div>
@@ -52,10 +76,14 @@ The weather dashboard will contain all the cities weather information and will b
           <div id='containerCardPortfolio' class="card h-100">
           <img src={photo4} alt="logo" />
             <div id='containerTextCard' class="card-body">
-              <a href="">
                 <h2 id='titleCard' class="card-title">JATE (PWA)</h2>
-                <p id='textCard' class="card-text">My task is to build a text editor that runs in the browser. The app will be a single-page application that meets the PWA criteria and it will feature a number of data persistence techniques. Additionally, the app will function offline.</p>
-              </a>
+                <p id='textCard' class="card-text">The app it's a text editor that runs in the browser and it's a single-page application that meets the PWA criteria. Additionally, the app will function offline.</p>
+                <div>
+                  <a id='linkPortfolio' href="https://pwa-text-editor-bhyv.onrender.com/">Deployed application</a>
+                </div>
+                <div>
+                  <a id='linkPortfolio' href="https://github.com/garmercy/-PWA-Text-Editor">GitHub repository</a>
+                </div>
             </div>
           </div>
         </div>
@@ -63,28 +91,19 @@ The weather dashboard will contain all the cities weather information and will b
           <div id='containerCardPortfolio' class="card h-100">
           <img src={photo5} alt="logo" />
             <div id='containerTextCard' class="card-body">
-              <a href="">
                 <h2 id='titleCard' class="card-title">Password Generator</h2>
                 <p id='textCard' class="card-text">This project was created with the goal of generate random passwords. </p>
-                <p id='textCard'>The main requirements are:</p>
-                <p id='textCard'>Generate a strong password that provides greater security.</p>
-                <p id='textCard'>Run in the browser, and the password can include special characters.</p>
-                
-              </a>
+                <p id='textCard'>This app runs in the browser, and the password can include special characters.</p>
+                <div>
+                  <a id='linkPortfolio' href="https://garmercy.github.io/PasswordGenerator/">Deployed application</a>
+                </div>
+                <div>
+                  <a id='linkPortfolio' href="https://github.com/garmercy/PasswordGenerator">GitHub repository</a>
+                </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div id='containerCardPortfolio' class="card h-100">
-          <img src={photo6} alt="logo" />
-            <div id='containerTextCard' class="card-body">
-              <a href="">
-                <h2 id='titleCard' class="card-title">Project 1</h2>
-                <p id='textCard' class="card-text">Babysitters Club is a community-driven platform designed to facilitate a trusted and secure environment for parents to exchange babysitting services.</p>
-              </a>
-            </div>
-          </div>
-        </div>
+        
       </div>
     </div>
   </section>
