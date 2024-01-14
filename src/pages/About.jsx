ec3b10c70f715ef284abc3cf428e7a1ff5612fe1
Cristina Garcia <garmercy.mar@gmail.com>
2024-01-14 12:05:27 +1100

    adding icons about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -36,6 +36,69 @@ export default function About() {
         Using a combination of cutting-edge technologies and reliable open-source software I build user-focused, performant apps and websites for smartphones, tablets, and desktops.
         </p>
       </div>
+      <div id="appsContainer" class="row">
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" alt="css" id="appIcon"/>
+
+            <p class="card-text">CSS</p>
+          </div>
+        </div>
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png" alt="css" id="appIcon"/>
+            <p class="card-text">Express</p>
+          </div>
+        </div>
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="css" id="appIcon"/>
+            <p class="card-text">Node</p>
+          </div>
+        </div>
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png" alt="css" id="appIcon"/>
+            <p class="card-text">React</p>
+          </div>
+        </div>
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="css" id="appIcon"/>
+            <p class="card-text">HTML</p>
+          </div>
+        </div>
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" alt="css" id="appIcon"/>
+            <p class="card-text">Bootstrap</p>
+          </div>
+        </div>
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="css" id="appIcon"/>
+            <p class="card-text">JavaScript</p>
+          </div>
+        </div>
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" alt="css" id="appIcon"/>
+            <p class="card-text">Git</p>
+          </div>
+        </div>
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg" alt="css" id="appIcon"/>
+            <p class="card-text">MySQL.</p>
+          </div>
+        </div>
+        <div id="containeMiniApp" class="col-sm-2 mb-3">
+          <div class="card-body">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg" alt="css" id="appIcon"/>
+            <p class="card-text">MongoDb</p>
+          </div>
+        </div>
+      </div>
     </div>
     
   );
