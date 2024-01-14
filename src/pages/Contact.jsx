f1d39b501d0b72c9062e5d89cbb50bac2ff6fafa
Cristina Garcia <garmercy.mar@gmail.com>
2024-01-14 12:18:16 +1100

    contact page form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,30 @@
 export default function Contact() {
-    return (
-        <h1>contact</h1>
+  return (
+    <div className="container mt-5">
+    <h1>Contact Us</h1>
+    <form>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label" id="contactForm">
+          Name
+        </label>
+        <input type="text" className="form-control" id="name" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label" id="contactForm">
+          Email
+        </label>
+        <input type="email" className="form-control" id="email" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="message" className="form-label" id="contactForm">
+          Message
+        </label>
+        <textarea className="form-control" id="message" rows="4"></textarea>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+    </div>
   );
 }
