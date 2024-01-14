9e1532d918be8c3dd8355bfda8fd2ed682a5ec1f
Cristina Garcia <garmercy.mar@gmail.com>
2024-01-14 12:03:15 +1100

    adding text about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,6 +16,27 @@ export default function About() {
           <img src={photo} alt="logo" id='photo'/>
         </div>
       </div>
+      <h1>About me</h1>
+      <div class="col-lg-12">
+        <h2 id="h1AboutMe" >About me</h2> 
+      </div>
+      <div >
+        <p id="aboutText">
+        I'm a self-taught web developer and Mobile App Developer with experience in designing new features from ideation to production, implementation of wireframes and design flows into high performance software applications. 
+        I take into consideration the user experience while writing reusable and efficient code. 
+        I passionately combine good design, technology, and innovation in all my projects, which I like to accompany from the first idea to release.
+        Currently, I'm focused on the backend development.
+        </p>
+      </div>
+      <div class="col-lg-12">
+        <h2 id="h1AboutMe">Technologies and Tools</h2> 
+      </div>
+      <div >
+        <p id="aboutText">
+        Using a combination of cutting-edge technologies and reliable open-source software I build user-focused, performant apps and websites for smartphones, tablets, and desktops.
+        </p>
+      </div>
     </div>
+    
   );
 }
