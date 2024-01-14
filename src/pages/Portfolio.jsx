ad8ba5fc2800e95303ad6e6f84b4912a9506da86
Cristina Garcia <garmercy.mar@gmail.com>
2024-01-14 12:22:25 +1100

    adding portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,78 @@
+import photo1 from '../assets/img/23.png';
 export default function Portfolio() {
     return (
-        <h1>portfolio</h1>
+    <section id="portfolio" class=" py-5">
+    <div>
+      <h1 id='h1Portfolio' class="text-center display-4">Portfolio</h1>
+      <div class="row">
+        <div class="col-lg-4 col-md-6 mb-3">
+          <div id='containerCardPortfolio' class="card h-100">
+          <img src={photo1} alt="logo" />
+            <div id='containerTextCard' class="card-body">
+              <a href="">
+                <h2 id='titleCard' class="card-title">Project 1</h2>
+                <p id='textCard' class="card-text">Description of Project 1 goes here.</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div id='containerCardPortfolio' class="card h-100">
+          <img src={photo1} alt="logo" />
+            <div id='containerTextCard' class="card-body">
+              <a href="">
+                <h2 id='titleCard' class="card-title">Project 1</h2>
+                <p id='textCard' class="card-text">Description of Project 1 goes here.</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div id='containerCardPortfolio' class="card h-100">
+          <img src={photo1} alt="logo" />
+            <div id='containerTextCard' class="card-body">
+              <a href="">
+                <h2 id='titleCard' class="card-title">Project 1</h2>
+                <p id='textCard' class="card-text">Description of Project 1 goes here.</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div id='containerCardPortfolio' class="card h-100">
+          <img src={photo1} alt="logo" />
+            <div id='containerTextCard' class="card-body">
+              <a href="">
+                <h2 id='titleCard' class="card-title">Project 1</h2>
+                <p id='textCard' class="card-text">Description of Project 1 goes here.</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div id='containerCardPortfolio' class="card h-100">
+          <img src={photo1} alt="logo" />
+            <div id='containerTextCard' class="card-body">
+              <a href="">
+                <h2 id='titleCard' class="card-title">Project 1</h2>
+                <p id='textCard' class="card-text">Description of Project 1 goes here.</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div id='containerCardPortfolio' class="card h-100">
+          <img src={photo1} alt="logo" />
+            <div id='containerTextCard' class="card-body">
+              <a href="">
+                <h2 id='titleCard' class="card-title">Project 1</h2>
+                <p id='textCard' class="card-text">Description of Project 1 goes here.</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   );
 }
